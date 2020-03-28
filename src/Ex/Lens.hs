77248869a449ef2eccdc4873e5a091620567de55
Brandon Elam Barker <beb82@cornell.edu>
2020-03-28 01:17:31 +0000

    more Lens exercises; on Lens laws

--- src/Ex/Lens.hs
+++ src/Ex/Lens.hs
@@ -10,6 +10,7 @@
 module Ex.Lens where
 
 import Control.Lens
+import Control.Lens.Unsound (lensProduct)
 import Control.Applicative
 import Data.Char
 import qualified Data.Map as M
@@ -35,10 +36,48 @@ purplePearl = Ship {
 , _numCrew = 38
 }
 
-conditional :: Lens' (Bool, a, a) a
-conditional = lens
-  (\s -> if view _1 s then view _2 s else view _3 s)
-  (\s a -> if view _1 s then set _2 a s else set _3 a s)
+---
+
+type UserName = String
+type UserId = String
+data Session = Session {
+  _userId :: UserId
+, _userName :: UserName
+, _createdTime :: String
+, _expiryTime :: String
+} deriving (Show, Eq)
+makeLenses ''Session
+
+-- Note: the input lenses must be disjoint for the
+-- lens product to be lawful
+userInfo :: Lens' Session (UserId, UserName)
+userInfo = lensProduct userId userName
+-- Not disjoint:
+alongsideUserId :: Lens' Session (Session, UserId)
+alongsideUserId = lensProduct id userId
+
+---
+
+data Err =
+    ReallyBadError { _msg :: String }
+  | ExitCode { _code :: Int }
+  deriving (Show, Eq)
+
+-- fail get-set but pass set-get and set-set
+msg :: Lens' Err String
+msg = lens getMsg setMsg
+  where
+    getMsg (ReallyBadError message) = message
+    -- Hrmm, I guess we just return ""?
+    getMsg (ExitCode _) = ""
+    setMsg (ReallyBadError _) newMessage = ReallyBadError newMessage
+    -- Nowhere to set it, I guess we do nothing?
+    setMsg (ExitCode n) newMessage = ExitCode n
+
+---
+-- break 2nd and third law:
+addCrewLens :: Lens' Ship Int
+addCrewLens = lens _numCrew (\ship nc -> ship{_numCrew = nc +  _numCrew ship})
 
 lensTests :: IO ()
 lensTests = do
@@ -46,3 +85,20 @@ lensTests = do
   let newPearl = set numCrew 41 purplePearl
   putStrLn $ show newPearl
   putStrLn $ show $ over numCrew (+3) purplePearl
+  let session = Session "1234" "Joe Blo" "2019-07-25" "2019-08-25"
+  putStrLn $ show $ view userInfo session
+  let newSession = session{_userId="5678"}
+  putStrLn $ show $ view alongsideUserId $
+    (set alongsideUserId (newSession, "9999") session)
+  putStrLn "The next two should break law get-set by =/=:"
+  putStrLn $ show $ set addCrewLens (view addCrewLens purplePearl) purplePearl
+  putStrLn $ show $ purplePearl
+  putStrLn "The next two should break law set-set by =/=:"
+  putStrLn $ show $ set addCrewLens 100 (set addCrewLens 1 purplePearl)
+  putStrLn $ show $ set addCrewLens 100 purplePearl
+
+
+conditional :: Lens' (Bool, a, a) a
+conditional = lens
+  (\s -> if view _1 s then view _2 s else view _3 s)
+  (\s a -> if view _1 s then set _2 a s else set _3 a s)
